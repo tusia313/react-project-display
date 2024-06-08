062907a6f30c61cc19109694c01f5d6cdd432f62
Marta Łoskot <loskotmarta@gmail.com>
2024-06-08 18:10:08 +0200

    Variables in jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-function App() {
+import CustomHeader from "./components/CustomHeader" 
+
+const App = () => {
+  let isLoggenIn = false
   return (
-    <div>
+    <div style = {{ backgroundColor : isLoggenIn ? "green" : "violet"}}>
+    <CustomHeader/>
+    <p className="subtitle">{isLoggenIn ? "Welcome back !" : "Please log in."}</p>
+    {!isLoggenIn && <button>Sign in</button>}
     </div>
   )
 }
---
+++ src/components/CustomHeader.js
@@ -0,0 +1,5 @@
+const CustomHeader = () => {
+    const title = "My App"
+    return <h1>{title} is lazy !</h1>
+}
+export default CustomHeader
